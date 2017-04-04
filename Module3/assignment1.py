b236d65155c64f424d5c6df676e679239f140dff
slysylvia <sylviaxh@gmail.com>
2017-04-03 21:43:39 -0700

    histogram exercise

--- Module3/assignment1.py
+++ Module3/assignment1.py
@@ -11,30 +11,27 @@ plt.style.use('ggplot')
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
 # 
-# .. your code here ..
-
+seeds_dataset = pd.read_csv('Datasets/wheat.data')
 
 #
 # TODO: Create a slice of your dataframe (call it s1)
 # that only includes the 'area' and 'perimeter' features
 # 
-# .. your code here ..
-
+s1 = seeds_dataset[['area', 'perimeter']]
 
 #
 # TODO: Create another slice of your dataframe (call it s2)
 # that only includes the 'groove' and 'asymmetry' features
 # 
-# .. your code here ..
-
+s2 = seeds_dataset[['groove', 'asymmetry']]
 
 #
 # TODO: Create a histogram plot using the first slice,
 # and another histogram plot using the second slice.
 # Be sure to set alpha=0.75
 # 
-# .. your code here ..
-
+s1.plot.hist(alpha=0.5)
+s2.plot.hist(alpha=0.5)
 
 # Display the graphs:
 plt.show()
