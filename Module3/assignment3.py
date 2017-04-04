8c10c192b35400705f3a7f006af33ab323ea48bc
slysylvia <sylviaxh@gmail.com>
2017-04-03 22:26:44 -0700

    3d scatterplots

--- Module3/assignment3.py
+++ Module3/assignment3.py
@@ -12,8 +12,7 @@ plt.style.use('ggplot')
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
 # 
-# .. your code here ..
-
+seeds_dataset = pd.read_csv('Datasets/wheat.data', index_col = 0)
 
 
 fig = plt.figure()
@@ -25,10 +24,15 @@ fig = plt.figure()
 # optional display parameter c='red', and also label your
 # axes
 # 
-# .. your code here ..
+s1 = fig.add_subplot(111, projection = '3d')
+s1.set_xlabel('area')
+s1.set_ylabel('perimeter')
+s1.set_zlabel('asymmetry')
+
+s1.scatter(seeds_dataset['area'], seeds_dataset['perimeter'], seeds_dataset['asymmetry'], c = 'r', marker = '.')
+plt.show()
 
 
-fig = plt.figure()
 #
 # TODO: Create a new 3D subplot using fig. Then use the
 # subplot to graph a 3D scatter plot using the width,
@@ -36,8 +40,14 @@ fig = plt.figure()
 # optional display parameter c='green', and also label your
 # axes
 # 
-# .. your code here ..
+fig = plt.figure()
 
+s2 = fig.add_subplot(111, projection = '3d')
+s2.set_xlabel('width')
+s2.set_ylabel('groove')
+s2.set_zlabel('length')
+
+s2.scatter(seeds_dataset['width'], seeds_dataset['groove'], seeds_dataset['length'], c = 'g', marker = '.')
 
 plt.show()
 
