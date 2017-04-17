48d9008e35f8d94e98c485e680f568a15b7dd274
sly-ninja <sylviaxh@gmail.com>
2017-04-16 18:32:17 -0700

    working on isomap exercise

--- Module4/assignment4.py
+++ Module4/assignment4.py
@@ -20,7 +20,7 @@ def Plot2D(T, title, x, y, num_to_plot=40):
     img_num = int(random.random() * num_images)
     x0, y0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.
     x1, y1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.
-    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    img = df.loc[i,:].values.reshape(num_pixels, num_pixels)
     ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
   # It also plots the full scatter:
@@ -38,8 +38,7 @@ num_pixels = int(math.sqrt(num_pixels))
 
 # Rotate the pictures, so we don't have to crane our necks:
 for i in range(num_images):
-  df.loc[i,:] = df.loc[i,:].reshape(num_pixels, num_pixels).T.reshape(-1)
-
+  df.loc[i,:] = df.loc[i,:].values.reshape(num_pixels, num_pixels).T.reshape(-1)
 
 #
 # TODO: Implement PCA here. Reduce the dataframe df down
@@ -51,22 +50,54 @@ for i in range(num_images):
 # x is the principal component you want displayed on the x-axis, Can be 0 or 1
 # y is the principal component you want displayed on the y-axis, Can be 1 or 2
 #
-# .. your code here ..
+from sklearn.decomposition import PCA
 
+pca = PCA(n_components=2, svd_solver='full')
+pca.fit(df)
+T = pca.transform(df)
+
+Plot2D(T, 'title', 0, 1, num_to_plot = 40)
 
 #
 # TODO: Implement Isomap here. Reduce the dataframe df down
 # to THREE components. Once you've done that, call Plot2D using
 # the first two components.
 #
-# .. your code here ..
+from sklearn import manifold
 
+iso = manifold.Isomap(n_neighbors = 4, n_components = 3)
+iso.fit(df)
+manifold = iso.transform(df)
+df.shape
+manifold.shape
+
+Plot2D(manifold, 'title', 0, 1, num_to_plot = 40)
 
 #
 # TODO: If you're up for a challenge, draw your dataframes in 3D
 # Even if you're not, just do it anyway.
 #
-# .. your code here ..
+def Plot3D(T, title, x, y, z, num_to_plot=40):
+  # This method picks a bunch of random samples (images in your case)
+  # to plot onto the chart:
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection = '3d')
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  #ax.set_zlabel('Component: {0}'.format(z))
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  #z_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.
+    x1, y1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.
+    
+    img = df.loc[i,:].values.reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
+  # It also plots the full scatter:
+  ax.scatter(T[:,x],T[:,y], marker='.',alpha=0.7)
 
 plt.show()
