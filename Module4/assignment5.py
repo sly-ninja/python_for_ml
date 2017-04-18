f84b9c565e6fcab34404b075dc673da998447439
slysylvia <sylviaxh@gmail.com>
2017-04-17 17:55:04 -0700

    still need to figure out how to add colors

--- Module4/assignment5.py
+++ Module4/assignment5.py
@@ -14,8 +14,11 @@ plt.style.use('ggplot')
 # TODO: Start by creating a regular old, plain, "vanilla"
 # python list. You can call it 'samples'.
 #
-# .. your code here .. 
+import os
 
+samples = []
+samples_i = []
+colors = []
 #
 # TODO: Write a for-loop that iterates over the images in the
 # Module4/Datasets/ALOI/32/ folder, appending each of them to
@@ -23,12 +26,18 @@ plt.style.use('ggplot')
 # temporary NDArray, just as shown in the Feature
 # Representation reading.
 #
+files = os.listdir('Datasets/ALOI/32')
+
+for file_name in files:
+  img = misc.imread( os.path.join('Datasets/ALOI/32', file_name) )
+  file_name.colors = 'b'
+  samples.append(  (img[::2, ::2] / 255.0).reshape(-1)  )
+    
 # Optional: Resample the image down by a factor of two if you
 # have a slower computer. You can also convert the image from
 # 0-255  to  0.0-1.0  if you'd like, but that will have no
 # effect on the algorithm's results.
 #
-# .. your code here .. 
 
 
 #
@@ -38,22 +47,26 @@ plt.style.use('ggplot')
 # in the Module4/Datasets/ALOI/32_i directory. Re-run your
 # assignment and answer the final question below.
 #
-# .. your code here .. 
+files_i = os.listdir('Datasets/ALOI/32i')
 
-
+for file_name in files_i:
+  img = misc.imread( os.path.join('Datasets/ALOI/32i', file_name) )
+  samples_i.append(  (img[::2, ::2] / 255.0).reshape(-1)  )
+  samples.extend(samples_i)
 #
 # TODO: Convert the list to a dataframe
 #
-# .. your code here .. 
-
-
-
+samples = pd.DataFrame( samples )
 #
 # TODO: Implement Isomap here. Reduce the dataframe df down
 # to three components, using K=6 for your neighborhood size
 #
-# .. your code here .. 
+from sklearn import manifold
 
+iso = manifold.Isomap(n_neighbors = 6, n_components = 3)
+iso.fit(samples)
+manifold = iso.transform(samples)
+manifold = pd.DataFrame( manifold )
 
 
 #
@@ -61,10 +74,8 @@ plt.style.use('ggplot')
 # can use either 'o' or '.' as your marker. Graph the first two
 # isomap components
 #
-# .. your code here .. 
 
-
-
+manifold.plot.scatter( 0, 1, c = colors )
 
 #
 # TODO: Create a 3D Scatter plot to graph your manifold. You
